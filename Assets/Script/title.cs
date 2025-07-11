ccbcb117e6dff28a684ed9fa7ae273ceaab6e5f2
SDHS-limited <wooozem@gmail.com>
2025-07-11 16:04:02 +0900

    메인타이틀

---
+++ Assets/Script/title.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title : MonoBehaviour
+{
+    [SerializeField] GameObject setting;
+    private void Start()
+    {
+        setting.SetActive(false);
+    }
+    public void gamestart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Setting()
+    {
+        setting.SetActive(true);
+    }
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+    public void X()
+    {
+        setting.SetActive(false);
+    }
+}
