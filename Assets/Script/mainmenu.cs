10188299174d9651289c7562a432609f1d71318d
SDHS-limited <wooozem@gmail.com>
2025-07-11 15:16:56 +0900

    메인타이틀 구성

---
+++ Assets/Script/mainmenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class mainmenu : MonoBehaviour
+{
+    [SerializeField] GameObject setting;
+    private void Start()
+    {
+        setting.gameObject.SetActive(false);
+    }
+    public void gameStart()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void Setting()
+    {
+        setting.gameObject.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void X()
+    {
+        setting.gameObject.SetActive(false);
+    }
+}
